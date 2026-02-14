ba5f2ad74cecb6b4a1bf876ec73add999ca090e7
Max Levine <maxdaniellevine@gmail.com>
2026-02-14 12:11:59 -0500

    entropy calcs

---
+++ scripts/entropy.js
@@ -0,0 +1,11 @@
+class Entropy {
+	k = 10; // number of partitions per box, must be >= total num of particles
+	totalEntropy;
+
+	// calculates entropy as the log of the number of microstates
+	calcEntropy(box) {
+		n = box.numParticles;
+		b = box.particleCounts["blue"];
+		return n*Math.log(n/(n-k)) + k*Math.log((n-k)/(k-b)) + b*Math.log((k-b)/b);
+	}
+}
